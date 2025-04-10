72d87d9ac6d13fcb7e3bc80e6f8a8c72aa7be87b
Tyler Hill <Tyler.Hill@FloridaDEP.gov>
2025-04-10 09:16:33 -0400

    updates to table descriptions 04-10-2025

--- TrendText/tableDescriptions/run.R
+++ TrendText/tableDescriptions/run.R
@@ -6,14 +6,19 @@ knitr::opts_chunk$set(warning = FALSE, echo = FALSE, message = FALSE)
 wd <- dirname(getActiveDocumentContext()$path)
 setwd(wd)
 
-# The lines below render the indiviudal reports and provide an output file
-rmarkdown::render("WC_Discrete_Trend_Text.Rmd")
-rmarkdown::render("WC_Continuous_Trend_Text.Rmd")
+# The lines below render the individual reports and provide an output file
+rmarkdown::render("SAV_TableDescriptions.Rmd")
+rmarkdown::render("WC_Discrete_TableDescriptions.Rmd")
+rmarkdown::render("WC_Continuous_TableDescriptions.Rmd")
+rmarkdown::render("WC_Nekton_TableDescriptions.Rmd")
+rmarkdown::render("Coral_TableDescriptions.Rmd")
+rmarkdown::render("CW_TableDescriptions.Rmd")
 
 # That output file is then loaded to produce these combined reports
 descTable <- setDT(read.xlsx(paste0("output/Atlas_Descriptions_", 
                                     gsub("_","-",(Sys.Date())), ".xlsx")))
-
+# Import WebsiteParameters.csv
+websiteParams <- fread("data/WebsiteParameters.csv")
 # Correct order of websiteParams to match format from the Atlas
 websiteParams <- websiteParams %>% 
   arrange(factor(IndicatorName, levels = c("Nutrients","Water Quality","Water Clarity")),
@@ -24,4 +29,4 @@ websiteParams <- websiteParams %>%
   filter(Website==1)
 setDT(websiteParams)
 
-rmarkdown::render("main.Rmd")
+rmarkdown::render("main.Rmd", output_file = paste0("output/allTableDescriptions_", Sys.Date(), ".docx"))
